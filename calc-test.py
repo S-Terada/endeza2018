7ebe4be55c62120bb219a820bb7f6202bea821db
S-Terada <shota.0626.1998@gmail.com>
2018-06-13 09:22:11 +0900

    update

--- calc-test.py
+++ calc-test.py
@@ -2,7 +2,7 @@
 #floatの座標データを取得し、正解のデータとの誤差を修正する
 
 import re
-from numpy import *
+import numpy as np
 
 #座標データを格納する関数
 def storeList(dataStr, data):
@@ -11,15 +11,9 @@ def storeList(dataStr, data):
             data.append(float(dataStr[i][j]))
     return data
 #座標値の誤差を計算する関数
-"""
-def calcCordinate(tuple1, tuple2):
-    ansx = (tuple1[0] - tuple2[0]) 
-    ansy = (cord21 - cord22) 
-    ans = (ansx + ansy) / 10
-    return ans
-"""
+
 #座標値の総和を軸ごとに計算し、タプルに格納
-def calcCordinate(cord):
+def calcCoordinate(cord):
     uprx = (cord[0][0] + cord[1][0] + cord[2][0] + cord[3][0])
     lowx = (cord[4][0] + cord[5][0] + cord[6][0] + cord[7][0])
 
@@ -42,21 +36,50 @@ def compGradient(masGrad, newGrad):
     return comp
 #xy座標を二重リストに格納する関数
 def storeTupleinList(data):
-    array = zeros((8, 3))
+    array = np.zeros((8, 2))
     k = 0
     i = 0
     j = 0
     for i in range(8):
         for j in range(3):
-            array[i][j] = data[k]
-            k += 1
+            if k % 3 != 2 :
+                array[i][j] = data[k]
+                k += 1
+            else:
+                k += 1
     #print(array)
     return array
+#首基準の相対座標に直す関数
+def reCoord(data):
+    neckx = data[1][0]
+    necky =data[1][1]
 
-new_data = open(r'teracolo.yml', "r", encoding='UTF-8')
-#new_data = open(r'terada_clo.yml', "r", encoding='UTF-8')
-#new_data = open(r'inoue_clo.yml', "r", encoding='UTF-8')
-master_data = open(r'takecolo.yml', "r", encoding='UTF-8')
+    for i in range(0, 8):
+        data[i][0] -= neckx
+        data[i][1] -= necky
+    return data
+#コロンビアチェック
+def checkColombia(mas, new):
+    mas1x = mas[7][0] - mas[4][0]
+    mas1y = mas[7][1] - mas[4][1]
+    mas2x = mas[6][0] - mas[3][0]
+    mas2y = mas[6][1] - mas[3][1]
+    new1x = new[7][0] - new[4][0]
+    new1y = new[7][1] - new[4][1]
+    new2x = new[6][0] - new[3][0]
+    new2y = new[6][1] - new[3][1]
+
+    preans1 = (mas1x * new1y) - (mas1y * new1x)
+    preans2 = (mas2x * new2y) - (mas2y * new2x)
+
+    ans = (preans1 + preans2) / 2
+
+    return ans
+
+new_data = open(r'sawaclo.yml', "r", encoding='UTF-8')
+master_data = open(r'daclo.yml', "r", encoding='UTF-8')
+#new_data = open(r'inoclo.yml', "r", encoding='UTF-8')
+#master_data = open(r'takeclo.yml', "r", encoding='UTF-8')
 lines = new_data.readlines()
 lines2 = master_data.readlines()
 i = 0
@@ -90,39 +113,22 @@ masterData = storeList(masterDataStr, masterData)
 mas = [[]]
 new = [[]]
 
-mas = storeTupleinList(masterData)
-new = storeTupleinList(newData)
-#print(mas[0])       #printデバッグ用
-#print(mas[0][1])    #printデバッグ用
 
-#print(mas2) #printデバッグ用
-#print(mas3) #printデバッグ用
+mas = reCoord(storeTupleinList(masterData))
+new = reCoord(storeTupleinList(newData))
+#print(mas[2])       #printデバッグ用
+#print(mas[5])    #printデバッグ用
+
 
 #ここから計算パート
-"""
-grad1 = Gradient(mas2, mas3)
-grad2 = Gradient(new2, new3)
 
-print(grad1)        #printデバッグ用
-print(grad2)        #printデバッグ用
 
-gradpers = compGradient(grad1, grad2)
-gradpers2 = compGradient(grad2, grad1)
+cans = checkColombia(mas, new)
 
-print(gradpers)     #printデバッグ用
-print(gradpers2)    #printデバッグ用
-"""
-#masans = calcCordinate(mas)
-#newans = calcCordinate(new)
-#avex = (masans[0] - newans[0]) / 8
-#avey = (masans[1] - newans[1]) / 8
-print(new[3][1])
-print(new[2][1])
 
-if new[3][1] < new[2][1] and new[4][1] < new[3][1] and new[6][1] < new[5][1] and new[7][1] < new[6][1]:
-        print("True")
-
-#print(masans)      #printデバッグ用
-#print(newans)      #printデバッグ用
-#print(avex)        #printデバッグ用
-#print(avey)        #printデバッグ用
+if abs(cans) < 3000 :
+    print("Pose Correct")
+    print("The door will unlock")
+else:
+    print("Pose Incorrect")
+    print("Go Back to your Home")
