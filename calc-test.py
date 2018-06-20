e1a5b69634f8d51c7591fdbf3c8a975b3ea830e3
S-Terada <shota.0626.1998@gmail.com>
2018-06-20 11:14:45 +0900

    pose9種類に対応

--- calc-test.py
+++ calc-test.py
@@ -58,8 +58,251 @@ def reCoord(data):
         data[i][0] -= neckx
         data[i][1] -= necky
     return data
-#コロンビアチェック
-def checkColombia(mas, new):
+
+def p1(mas, new):
+    mas1x = mas[4][0] - mas[3][0]
+    mas1y = mas[4][1] - mas[3][1]
+    mas2x = mas[3][0] - mas[2][0]
+    mas2y = mas[3][1] - mas[2][1]
+    mas3x = mas[7][0] - mas[5][0]
+    mas3y = mas[7][1] - mas[5][1]
+
+    new1x = new[4][0] - new[3][0]
+    new1y = new[4][1] - new[3][1]
+    new2x = new[3][0] - new[2][0]
+    new2y = new[3][1] - new[2][1]
+    new3x = new[7][0] - new[5][0]
+    new3y = new[7][1] - new[5][1]
+
+    #mpans : mas pre ans
+    mpans1 = (mas1x * mas2x) + (mas1y * mas2y)  #dot
+    mpans2 = (mas2x * mas3y) - (mas2y * mas3x)  #cross
+
+    #npans : new pre ans
+    npans1 = (new1x * new2x) + (new1y * new2y)  #dot
+    npans2 = (new2x * new3y) - (new2y * new3x)  #cross
+
+    ans = (abs((mpans1 - npans1)) + abs((mpans2 - npans2))) / 2
+
+    return ans
+
+def p2(mas, new):
+    mas1x = mas[7][0] - mas[6][0]
+    mas1y = mas[7][1] - mas[6][1]
+    mas2x = mas[6][0] - mas[5][0]
+    mas2y = mas[6][1] - mas[5][1]
+    mas3x = mas[4][0] - mas[2][0]
+    mas3y = mas[4][1] - mas[2][1]
+
+    new1x = new[7][0] - new[6][0]
+    new1y = new[7][1] - new[6][1]
+    new2x = new[6][0] - new[5][0]
+    new2y = new[6][1] - new[5][1]
+    new3x = new[4][0] - new[2][0]
+    new3y = new[4][1] - new[2][1]
+
+    #mpans : mas pre ans
+    mpans1 = (mas1x * mas2x) + (mas1y * new2y)  #dot
+    mpans2 = (mas2x * mas3y) - (mas2y * mas3x)  #cross
+
+    #npans : new pre ans
+    npans1 = (new1x * new2x) + (new1y * new2y)  #dot
+    npans2 = (new2x * new3y) - (new2y * new3x)  #cross
+    ans = (abs((mpans1 - npans1)) + abs((mpans2 - npans2))) / 2
+
+    return ans
+
+def p3(mas, new):
+    mas1x = mas[4][0] - mas[2][0]
+    mas1y = mas[4][1] - mas[2][1]
+    mas2x = mas[7][0] - mas[5][0]
+    mas2y = mas[7][1] - mas[5][1]
+    new1x = new[4][0] - new[2][0]
+    new1y = new[4][1] - new[2][1]
+    new2x = new[7][0] - new[5][0]
+    new2y = new[7][1] - new[5][1]
+
+    #mpans : mas pre ans
+    mpans = (mas1x * mas2y) - (mas1y * mas2x)  #cross
+
+    #npans : new pre ans
+    npans = (new1x * new2y) - (new1y * new2x)  #cross
+
+    #additional condition determination
+    if new[7][1] > new[6][1] and new[3][1] > new[4][1] and new[6][1] > new[3][1]:
+        constant = 0
+    else:
+        constant = 9999
+
+    ans = (abs(mpans + npans) + constant) / 2
+
+    return ans
+
+def p4(mas, new):
+    mas1x = mas[4][0] - mas[3][0]
+    mas1y = mas[4][1] - mas[3][1]
+    mas2x = mas[3][0] - mas[2][0]
+    mas2y = mas[3][1] - mas[2][1]
+    mas3x = mas[7][0] - mas[6][0]
+    mas3y = mas[7][1] - mas[6][1]
+    mas4x = mas[6][0] - mas[5][0]
+    mas4y = mas[6][1] - mas[5][1]
+    
+    new1x = new[4][0] - new[3][0]
+    new1y = new[4][1] - new[3][1]
+    new2x = new[3][0] - new[2][0]
+    new2y = new[3][1] - new[2][1]
+    new3x = new[7][0] - new[6][0]
+    new3y = new[7][1] - new[6][1]
+    new4x = new[6][0] - new[5][0]
+    new4y = new[6][1] - new[5][1]
+
+    #mpans : mas pre ans
+    mpans1 = (mas1x * mas2x) + (mas1y * mas2y)  #dot
+    mpans2 = (mas3x * mas4x) + (mas3y * mas4y)  #dot
+
+    #npans : new pre ans
+    npans1 = (new1x * new2x) + (new1y * new2y)  #dot
+    npans2 = (new3x * new4x) + (new3y * new4y)  #dot
+
+    #additional condition determination
+    if new[3][1] > new[4][1] and new[6][1] > new[7][1]:
+        constant = 0
+    else:
+        constant = 9999
+
+    ans = (abs((mpans1 - npans1)) + abs((mpans2 - npans2)) + constant) / 2
+
+    return ans
+
+def p5(mas, new):
+    mas1x = mas[4][0] - mas[2][0]
+    mas1y = mas[4][1] - mas[2][1]
+    mas2x = mas[7][0] - mas[5][0]
+    mas2y = mas[7][1] - mas[5][1]
+    new1x = new[4][0] - new[2][0]
+    new1y = new[4][1] - new[2][1]
+    new2x = new[7][0] - new[5][0]
+    new2y = new[7][1] - new[5][1]
+
+    #mpans : mas pre ans
+    mpans = (mas1x * mas2y) - (mas1y * mas2x)  #cross
+
+    #npans : new pre ans
+    npans = (new1x * new2y) - (new1y * new2x)  #cross
+
+    #additional condition determination
+    if new[7][1] > new[5][1] and new[4][1] > new[2][1] and new[3][1] > new[5][1]:
+        constant = 0
+    else:
+        constant = 9999
+
+    ans = (abs(mpans + npans) + constant) / 2
+
+    return ans
+
+def p6(mas, new):
+    mas1x = mas[4][0] - mas[2][0]
+    mas1y = mas[4][1] - mas[2][1]
+    mas2x = mas[7][0] - mas[5][0]
+    mas2y = mas[7][1] - mas[5][1]
+    new1x = new[4][0] - new[2][0]
+    new1y = new[4][1] - new[2][1]
+    new2x = new[7][0] - new[5][0]
+    new2y = new[7][1] - new[5][1]
+
+    #mpans : mas pre ans
+    mpans = (mas1x * mas2y) - (mas1y * mas2x)  #cross
+
+    #npans : new pre ans
+    npans = (new1x * new2y) - (new1y * new2x)  #cross
+
+    #additional condition determination
+    if new[4][1] > new[3][1] and new[3][1] > new[6][1] and new[6][1] > new[7][1]:
+        constant = 0
+    else:
+        constant = 9999
+
+    ans = (abs(mpans + npans) + constant) / 2
+
+    return ans
+
+def p7(mas, new):
+    mas1x = mas[4][0] - mas[3][0]
+    mas1y = mas[4][1] - mas[3][1]
+    mas2x = mas[3][0] - mas[2][0]
+    mas2y = mas[3][1] - mas[2][1]
+    mas3x = mas[7][0] - mas[6][0]
+    mas3y = mas[7][1] - mas[6][1]
+    mas4x = mas[6][0] - mas[5][0]
+    mas4y = mas[6][1] - mas[5][1]
+
+    new1x = new[4][0] - new[3][0]
+    new1y = new[4][1] - new[3][1]
+    new2x = new[3][0] - new[2][0]
+    new2y = new[3][1] - new[2][1]
+    new3x = new[7][0] - new[6][0]
+    new3y = new[7][1] - new[6][1]
+    new4x = new[6][0] - new[5][0]
+    new4y = new[6][1] - new[5][1]
+
+    # mpans : mas pre ans
+    mpans1 = (mas1x * mas2x) + (mas1y * mas2y)  # dot
+    mpans2 = (mas3x * mas4x) + (mas3y * mas4y)  # dot
+
+    # npans : new pre ans
+    npans1 = (new1x * new2x) + (new1y * new2y)  # dot
+    npans2 = (new3x * new4x) + (new3y * new4y)  # dot
+
+    # additional condition determination
+    if new[4][1] > new[3][1] and new[6][1] > new[7][1]:
+        constant = 0
+    else:
+        constant = 9999
+
+    ans = (abs((mpans1 - npans1)) + abs((mpans2 - npans2)) + constant) / 2
+
+    return ans
+
+def p8(mas, new):
+    mas1x = mas[4][0] - mas[3][0]
+    mas1y = mas[4][1] - mas[3][1]
+    mas2x = mas[3][0] - mas[2][0]
+    mas2y = mas[3][1] - mas[2][1]
+    mas3x = mas[7][0] - mas[6][0]
+    mas3y = mas[7][1] - mas[6][1]
+    mas4x = mas[6][0] - mas[5][0]
+    mas4y = mas[6][1] - mas[5][1]
+
+    new1x = new[4][0] - new[3][0]
+    new1y = new[4][1] - new[3][1]
+    new2x = new[3][0] - new[2][0]
+    new2y = new[3][1] - new[2][1]
+    new3x = new[7][0] - new[6][0]
+    new3y = new[7][1] - new[6][1]
+    new4x = new[6][0] - new[5][0]
+    new4y = new[6][1] - new[5][1]
+
+    # mpans : mas pre ans
+    mpans1 = (mas1x * mas2x) + (mas1y * mas2y)  # dot
+    mpans2 = (mas3x * mas4x) + (mas3y * mas4y)  # dot
+
+    # npans : new pre ans
+    npans1 = (new1x * new2x) + (new1y * new2y)  # dot
+    npans2 = (new3x * new4x) + (new3y * new4y)  # dot
+
+    # additional condition determination
+    if new[3][1] > new[4][1] and new[6][1] > new[7][1]:
+        constant = 0
+    else:
+        constant = 9999
+
+    ans = (abs((mpans1 - npans1)) + abs((mpans2 - npans2)) + constant) / 2
+
+    return ans
+
+def p9(mas, new):
+#判定用ベクトル生成
     mas1x = mas[7][0] - mas[4][0]
     mas1y = mas[7][1] - mas[4][1]
     mas2x = mas[6][0] - mas[3][0]
@@ -68,7 +311,7 @@ def checkColombia(mas, new):
     new1y = new[7][1] - new[4][1]
     new2x = new[6][0] - new[3][0]
     new2y = new[6][1] - new[3][1]
-
+#外積計算
     preans1 = (mas1x * new1y) - (mas1y * new1x)
     preans2 = (mas2x * new2y) - (mas2y * new2x)
 
@@ -78,8 +321,6 @@ def checkColombia(mas, new):
 
 new_data = open(r'sawaclo.yml', "r", encoding='UTF-8')
 master_data = open(r'daclo.yml', "r", encoding='UTF-8')
-#new_data = open(r'inoclo.yml', "r", encoding='UTF-8')
-#master_data = open(r'takeclo.yml', "r", encoding='UTF-8')
 lines = new_data.readlines()
 lines2 = master_data.readlines()
 i = 0
@@ -98,32 +339,38 @@ for line in lines:  #新規データ
 for line in lines2: #マスターデータ
     masterDataStr.append(re.findall(pattern, line))
 
-#print(newDataStr)
-
 #新規データを一次元配列にfloat型で格納しなおす
 newData = storeList(newDataStr, newData)
 masterData = storeList(masterDataStr, masterData)
 
-#print(newData)    #printデバッグ用
-#print(masterData)   #printデバッグ用
-
-#体の部位ごとの座標をタプルに格納
-#スマートじゃないからどうにかしたい
-#スマートになった
 mas = [[]]
 new = [[]]
 
 
 mas = reCoord(storeTupleinList(masterData))
 new = reCoord(storeTupleinList(newData))
-#print(mas[2])       #printデバッグ用
-#print(mas[5])    #printデバッグ用
-
 
 #ここから計算パート
+#pose番号から実行する判定関数を決定する辞書
+dict_pose = {
+    1: p1(mas, new),
+    2: p2(mas, new),
+    3: p3(mas, new),
+    4: p4(mas, new),
+    5: p5(mas, new),
+    6: p6(mas, new),
+    7: p7(mas, new),
+    8: p8(mas, new),
+    9: p9(mas, new)
+    }
 
+key = 9 #実際には何らかの方法でkey(=pose番号)を外からもらう
 
-cans = checkColombia(mas, new)
+#keyを辞書に投げて関数を実行し，その結果をcansへ代入する
+if key in dict_pose:
+    cans = dict_pose[key]
+else:
+    print("do not work")
 
 
 if abs(cans) < 3000 :
