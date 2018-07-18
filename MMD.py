68f89140971583f0a5c30c2b7e7b3e21745d0edb
S-Terada <shota.0626.1998@gmail.com>
2018-07-18 13:22:40 +0900

    動作確認済み

--- MMD.py
+++ MMD.py
@@ -1,48 +1,36 @@
+# coding=utf-8
 import re
 import numpy as np
 import csv
 import random
+from mFunc import *
+from lFunc import *
 
-#座標データを格納する関数
-def storeList(dataStr, data):
-    for i in range(len(dataStr)):
-        for j in range(len(dataStr[i])):
-            data.append(float(dataStr[i][j]))
-    return data
-#xy座標を二重リストに格納する関数
-def storeDoubleList(data):
+def storeSrcList(data):
     array = np.zeros((8, 2))
     k = 0
     i = 0
     j = 0
     for i in range(8):
-        for j in range(3):
-            if k % 3 != 2 :
+        for j in range(2):
+            if k % 3 != 2:
                 array[i][j] = data[k]
                 k += 1
             else:
                 k += 1
-    #print(array)
+    # print(array)
     return array
-#首基準の相対座標に直す関数
-def reCoord(data):
-    neckx = data[1][0]
-    necky =data[1][1]
 
-    for i in range(0, 8):
-        data[i][0] -= neckx
-        data[i][1] -= necky
-    return data
 def makeDataforML(src):
     srcArray = [[]]
     #print(masArray)
 
-    for i in range(1, 50):
-        randnum = random.random()
+    for i in range(1, 51):
+        randnum = random.uniform(0.8, 1.2)
         srcArray = np.array(src * randnum)
-        with open('master{0}.csv'.format(i), 'w') as f:
+        with open('./outputData/master{0}.csv'.format(i), 'w') as f:
             writer = csv.writer(f, lineterminator='\n')
-            writer.writerows(src)
+            writer.writerows(srcArray)
 
     f.close()
 
@@ -59,9 +47,8 @@ srcDataStr = []
 srcData = []
 
 
-
 #正規表現のパターンをセット
-pattern = r"0\.|[0-9]*\.[0-9]*e[\+-\-][0-9]*"
+pattern = r"0\.|[0-9]*\.[0-9]*"
 
 #配列にデータを追加 1行を読み込んでパターンとマッチしたものを二次元配列に追加
 for line in lines1:
@@ -74,7 +61,7 @@ srcData = storeList(srcDataStr, srcData)
 srcresult = [[]]
 
 
-srcresult = reCoord(storeDoubleList(srcData))
+srcresult = reCoord(storeSrcList(srcData))
 
 #print(srcresult)
 makeDataforML(srcresult)
