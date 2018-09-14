a169f2424f0e5fbb2a1797d4937843e090148804
S-Terada <shota.0626.1998@gmail.com>
2018-09-14 16:18:11 +0900

    Update ピクトマン

--- pict4.py
+++ pict4.py
@@ -1,11 +1,9 @@
 from PIL import Image, ImageDraw, ImageFont
 
-im = Image.new("RGB", (512, 512), (128, 128, 128))
+im = Image.new("RGB", (512, 512), (100, 100, 100))
 
 draw = ImageDraw.Draw(im)
 
-#draw.line((0, im.height, im.width, 0), fill=(255, 0, 0), width=8)
-#draw.rectangle((100, 100, 200, 200), fill=(0, 255, 0))
 draw.ellipse((220, 100, 320, 200), fill=(255, 255, 255))    #Head
 draw.rectangle((220, 210, 320, 400), fill=(255, 255, 255))  #Body
 draw.rectangle((330, 210, 400, 250), fill=(255, 255, 255))  #RIght Arm till Elbow
